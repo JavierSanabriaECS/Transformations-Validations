43777b9a9060460641e5f1d6fa846492625669e1
javier <javier@ecsfinlatam.com>
2021-12-10 10:42:19 -0500

    MOD:Change according to unit test

--- src/test/java/com/ecs/latam/ValidationMT940Test.java
+++ src/test/java/com/ecs/latam/ValidationMT940Test.java
@@ -2,22 +2,28 @@ package com.ecs.latam;
 
 import com.ecs.latam.kafka.domain.ValidationMT940;
 import com.prowidesoftware.swift.utils.Lib;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 
 class ValidationMT940Test {
+    private  String MT940_OK;
+
+    @BeforeAll
+    void setup() throws IOException {
+
+        MT940_OK  = Lib.readResource("MT940_accounts.txt");
+
+    }
+
 
     @Test
-    void validarMT() {
-    }
-    @Test
-    public void entradaMensaje() throws IOException {
-       ValidationMT940 v = new ValidationMT940();
-        String inMsg ="";
-        inMsg = Lib.readResource("Ejemplo MT940 ScotiabankColombia.txt");
-        Assertions.assertNotEquals(inMsg,"");
-        v.validarMT(inMsg);
+    public void givenMT940_WhenFileIsOk_ThenValidationIsTrue() {
+        ValidationMT940 v = new ValidationMT940();
+
+        boolean isValidated = v.validarMT(MT940_OK);
+        assertThat(isValidated).isTrue();
     }
 }
